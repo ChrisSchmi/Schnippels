73887b6348babfe19f7fbcf8e45fb1f1e7a920fc
Christian <csc@sondev.de>
2015-11-12 10:03:38 +0100

    Introduced DataAndTime

---
+++ DateAndTime.cs
@@ -0,0 +1,26 @@
+public int GetQuarterOfYear()
+{
+	var month = DateTime.Now.Month;
+	
+	if(month < 4)
+	{
+		return 1;
+	}
+	
+	if(month > 3 && month < 7)
+	{
+		return 2;
+	}
+	
+	if(month > 6 && month < 10)
+	{
+		return 3;
+	}
+	
+	if(month > 9)
+	{
+		return 4;
+	}
+	
+	return 0;
+}
