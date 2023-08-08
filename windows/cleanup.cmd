aaf7cb9a0a18a5f61905cb07381b87411d198bab
Christian <mail@christianschmidt.info>
2023-08-08 09:36:23 +0200

    optimized cleanup script

--- windows/cleanup.cmd
+++ windows/cleanup.cmd
@@ -1,11 +1,22 @@
 @echo off
 
 cls
-echo navigation to %USERPROFILE%\AppData\Local\Temp
-cd %USERPROFILE%\AppData\Local\Temp
+echo navigation to " %USERPROFILE%\AppData\Local\Temp "
 
+C:
+cd \
+cd %USERPROFILE%
+cd AppData
+cd Local
+cd Temp
+
+echo ====== Deleting old *.log files ====== 
 del *.log
+
+echo ====== Deleting old *.tmp files ====== 
 del *.tmp
+
+echo ====== Deleting old *.dat files ====== 
 del *.dat
 del *.diagsession
 
