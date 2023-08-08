ca9c6afac6f2d75b4785be5c70fb7b501634ab70
Christian <mail@christianschmidt.info>
2023-08-08 09:11:03 +0200

    added useful scripts for windows

---
+++ windows/restart.cmd
@@ -0,0 +1,10 @@
+@echo off
+echo restart?
+timeout 25
+echo ciao :)
+
+rem 2022 Edition
+rem shutdown.exe /r /f /t 0
+
+rem 2023 Edition
+shutdown.exe /g /f /t 0
---
+++ windows/shutdown.cmd
@@ -0,0 +1,7 @@
+@echo off
+cls
+echo shutdown?
+timeout 25
+echo ciao :)
+
+shutdown.exe /s /f /t 0
